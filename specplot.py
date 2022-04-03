e6ced8191940346cc4c26096c68c5ca35e4acfa1
hypatiameraviglia <hbarry@cub.uca.edu>
2022-04-03 15:00:40 -0500

    Added column labels and imported matplotlib

--- specplot.py
+++ specplot.py
@@ -3,4 +3,14 @@
 import numpy as np
 
 #Load .csv into numpy array
-specdata = np.loadtxt('SN2014J.csv', delimiter=",", skiprows=
+specdata = np.loadtxt('1987Aspec.csv', delimiter=",", skiprows=1)
+
+#Handles for x and y solumns
+time = specdata[:,1]
+magnitude = specdata[:,2]
+
+#Grab pylab
+import pylab as plt
+
+plt.plot(time, magnitude)
+plt.xlabel(Time
